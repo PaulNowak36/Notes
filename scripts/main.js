f2513a4bcca698948cb85bc5492f39048a592020
PaulNowak36 <paul.nowak@algosup.com>
2022-11-15 09:40:56 +0100

    loading notes + fullscreen

--- scripts/main.js
+++ scripts/main.js
@@ -4,39 +4,70 @@ localStorage.setItem('school', 'ALGOSUP');
 
 let notes = [];
 
+const noteTxt = document.querySelector('#note-text');
+const createBtn = document.querySelector('#create-note');
+const clearBtn = document.querySelector('#clear-notes');
+
+const notesList = document.querySelector('#notes-list > tbody');
+
+const fullscreenBtn = document.querySelector('#fullscreen');
+const manageNotes = document.querySelector('#manage-notes');
+let isFullscreen = false;
+
+fullscreenBtn.addEventListener('click', () => {
+    if (isFullscreen) {
+        exitFullscreen();
+    } else {
+        fullscreen(document.body);
+    }
+    isFullscreen = !isFullscreen;
+});
+
+function fullscreen(element) {
+    if (element.requestFullscreen) element.requestFullscreen();
+    else if (element.webkitRequestFullscreen) element.webkitRequestFullscreen();
+    else if (element.msRequestFullscreen) element.msRequestFullscreen();
+    else if (element.mozRequestFullscreen) element.mozRequestFullscreen();
+}
+
+function exitFullscreen() {
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+    else if (document.mozExitFullscreen) document.mozExitFullscreen();
+}
+
 function loadNotes() {
     notes = localStorage.getItem('notes') || [];
     notes = JSON.parse(notes);
-    //for (let i = 0; i < notes.length; i++) {
+    for (let i = 0; i < notes.length; i++)
+    {
         const newRow = document.createElement('tr');
-        newRow.id = notes[i].id; //New
-    
+        newRow.id = notes[i].id; 
+
         const newNote = document.createElement('td');
-        newNote.textContent = noteTxt.text;
-        //notes.append(noteTxt.value);
-    
+        newNote.textContent = notes[i].text;
+
+
         const newActions = document.createElement('td');
         const delBtn = document.createElement('button');
-        delBtn.id = `del-${notes[i].id}`; //New
+        delBtn.id = `del-${notes[i].id}`; 
         delBtn.className = 'btn btn-danger';
-        delBtn.innerHTML = 'Delete'; /* '<i class="fa-solid fa-trash-can">';
-     */    delBtn.addEventListener('click', deleteNote);
+        delBtn.innerHTML = 'Delete'; 
+        delBtn.addEventListener('click', deleteNote);
         newActions.append(delBtn);
-    
+
         
         newRow.append(newNote);
         newRow.append(newActions);
         
         notesList.append(newRow);
         noteTxt.focus();
-    //}
+
+    }
 }
+loadNotes();
 
-const noteTxt = document.querySelector('#note-text');
-const createBtn = document.querySelector('#create-note');
-const clearBtn = document.querySelector('#clear-notes');
-
-const notesList = document.querySelector('#notes-list > tbody');
 
 
 createBtn.addEventListener('click', addNote);
